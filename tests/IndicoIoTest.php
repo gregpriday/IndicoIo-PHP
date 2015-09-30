26d901167c9ed04c5e4aecad5917e0cd270a36d8
Madison May <madison@indico.io>
2015-09-30 18:31:06 -0400

    ADD: threshold kwarg test

--- tests/IndicoIoTest.php
+++ tests/IndicoIoTest.php
@@ -103,6 +103,15 @@ class IndicoIoTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(count($keys_result), 10);
     }
 
+    public function testThreshold() 
+    {
+        $data = IndicoIo::text_tags('I want to move to New York City!', array("threshold"=>0.05));
+        $values = array_values($data);
+        for ($i = 0; $i < count($values); $i++) {
+            $this->assertGreaterThan(0.05, $values[$i]);
+        } 
+    }
+
     public function testNamedEntities()
     {
         self::skipIfMissingCredentials();
