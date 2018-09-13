09fd66b58bddfbcf3bf8e27e2d6af39067f6d507
Greg Priday <greg@siteorigin.com>
2018-09-13 11:05:51 +0200

    Added missing Multi code

--- IndicoIo/Utils/Multi.php
+++ IndicoIo/Utils/Multi.php
@@ -2,6 +2,32 @@
 
 namespace IndicoIo\Utils;
 
-class Multi{
+use Exception;
 
+Class Multi
+{
+    public static function filterApis($apis, $accepted) {
+        foreach ($apis as $api) {
+            if (!in_array($api, $accepted)) {
+                throw new Exception(
+                    $api
+                    + " is not an acceptable api name. Please use "
+                    + implode(",", $accepted)
+                );
+            }
+        }
+        return implode(",", $apis);
+    }
+    public static function convertResults($results, $apis) {
+        $converted_results = array();
+        foreach ($apis as $api) {
+            $response = $results[$api];
+            if (array_key_exists("results", $response)) {
+                $converted_results[$api] = $response["results"];
+            } else {
+                throw new Exception($api . " encountered an error: " . $response['error']);
+            }
+        }
+        return $converted_results;
+    }
 }
